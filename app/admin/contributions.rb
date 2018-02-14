b9b930ba9892dda65c4349dc26f97637e9b9ae86
John Neil Soo <neil.soo@adish.co.jp>
2018-02-14 14:25:40 +0800

    more features

--- app/admin/contributions.rb
+++ app/admin/contributions.rb
@@ -1,15 +1,13 @@
 ActiveAdmin.register Contribution do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
-
+  index do
+    selectable_column
+    id_column
+    column :player do |contrib|
+      contrib.player.in_game_name
+    end
+    column :contribution_type
+    column :value
+    column :created_at
+    actions
+  end
 end
--- app/admin/page_donations.rb
+++ app/admin/page_donations.rb
@@ -1,15 +1,14 @@
 ActiveAdmin.register PageDonation do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
-
+  index do
+    selectable_column
+    id_column
+    column :player do |pd|
+      pd.player.in_game_name
+    end
+    column :count
+    column :level
+    column :created_at
+    column :updated_at
+    actions
+  end
 end
